9b4c0cb5c510ebe03970d5d6f25715dd7f3a6025
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-06-14 19:28:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 function highlight() {
     //Write your code here
     
-const boldwords=document.querySelectorAll("p strong")
-boldwords.style.color="green"	
+const boldwords=document.querySelectorAll("strong")
+boldwords.forEach((tag) => {
+  tag.style.color = "green";
+  tag.style.fontWeight = "bold";
+});
+
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+ const boldwords=document.querySelectorAll("strong")
+boldwords.forEach((tag) => {
+  tag.style.color = "rgb(0, 0, 0)";
+  tag.style.fontWeight = "bold";
+});
     
 }
